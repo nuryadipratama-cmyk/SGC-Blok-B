08577d086b6c576a32ee796284ff4670c4233b49
nuryadipratama-cmyk <nuryadi.pratama@gmail.com>
2026-08-30 20:22:44 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'sgc-blok-b-v1';
+const ASSETS = [
+  '/SGC-Blok-B/',
+  '/SGC-Blok-B/index.html',
+  '/SGC-Blok-B/icon-192.png',
+  '/SGC-Blok-B/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
